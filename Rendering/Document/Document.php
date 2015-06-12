2482b2ae8fae0b516ba26c2fccd02bcf10a6070c
rs2487 <raphael.stocker@massiveart.com>
2015-06-12 12:25:52 +0200

    Added check if mime is suported

--- Rendering/Document/Document.php
+++ Rendering/Document/Document.php
@@ -2,6 +2,40 @@
 
 namespace Massive\MediaRenderingBundle\Rendering\Document;
 
+use Imagine\Image\ImageInterface;
+
 class Document
 {
+    /**
+     * @param string $source
+     * 
+     * @return ImageInterface
+     */
+    public function render($source)
+    {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($finfo, $source);
+        finfo_close($finfo);
+        
+        // check if mime type is supported
+        if ($this->supportsMimeType($mimeType)) {
+            
+        }
+    }
+
+    /**
+     * @param type $mimeType
+     * 
+     * @return boolean
+     */
+    public function supportsMimeType($mimeType)
+    {
+        $supported = false;
+        switch ($mimeType) {
+           case "application/pdf":
+               $supported = true;
+        }
+        
+        return $supported;
+    }
 }
